445f3429da3ace6a23a962c95c1fc3b3ddbfceca
Madina <madinatussupova0820@gmail.com>
2023-05-18 12:46:17 +0600

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
-            Madina Tussupova
+            Madina
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -59,8 +59,11 @@ const Navbar = () => {
                   key={link.key}
                   className={`${
                     active === link.title ? "text-white" : "text-secondary"
-                  } hover:text-white text-[18px] font-medium cursor-pointer`}
-                  onClick={() => setActive(link.title)}
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
                 >
                   <a href={`#${link.id}`}>{link.title}</a>
                 </li>

817e618cd4f7de689f5019b166288c67cf62de16
Madina <madinatussupova0820@gmail.com>
2023-05-22 14:49:07 +0600

    start works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,17 +7,40 @@ import SectionWrapper from "../hoc/SectionWrapper";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({index, name, description, tags, image,source_code_link}) => {
+  return (
+    <motion.div>
+      test
+    </motion.div>
+  )
+}
+
 const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          My works
-        </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Projects
-        </h2>
+        <p className={`${styles.sectionSubText} text-center`}>My works</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
       </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          These are the project I have completed to showcase my skills. Each
+          project contains a brief explanation and a link to the code
+          repository. It displays my ability to write clean code and grasp on
+          new technologies effectively.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} 
+          />
+        ))}
+      </div>
     </>
   );
 };

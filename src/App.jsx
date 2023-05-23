9075bbde0273510406d4014363bfe61f7652199a
Madina <madinatussupova0820@gmail.com>
2023-05-23 13:15:18 +0600

    contact section, earth model

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
         <About />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
